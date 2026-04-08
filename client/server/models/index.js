ab78f61f4a7b9c90e83c98cdb1d225e1e91a6479
SusuPay Engineer <engineering@susupay.com>
2026-04-08 17:03:08 +0000

    Vercel Migration: Cleanup Wasmer files and update API constants

--- client/server/models/index.js
+++ client/server/models/index.js
@@ -3,16 +3,27 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false,
-  }
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false,
+      }
+    );
 
 // Initialize Models
 const User = require('./User')(sequelize);
